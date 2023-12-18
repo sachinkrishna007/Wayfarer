a709eb3adf2ae338b292505ab2eadb47d25e0fab
sachinkrishna007 <iamsachinkrishna@gmail.com>
2023-12-18 18:48:50 +0530

    building err fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log("mongodb connected");
   } catch {
-    console.log(error.message);
+   
     process.exit(1);
   }
 };
